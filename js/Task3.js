b89efc0f826bfd66af2164fb8206903150544e93
Raihan <raihanromi98@gmail.com>
2022-10-23 03:56:34 +0600

    done

--- js/Task3.js
+++ js/Task3.js
@@ -26,17 +26,17 @@ const sortLines = () => {
 const reverse = () => {
   let input_value = document.getElementById("magic-input").value;
   let magicArray = input_value.split("\n");
-  console.log(magicArray)
-      var newmagicinput = "";
-      magicArray.reverse();
-      for (let i = 0; i < magicArray.length; i++) {
-        if (i == magicArray.length - 1) {
-          newmagicinput += magicArray[i];
-        } else {
-          newmagicinput += magicArray[i] + "\n";
-        }
-      }
-      document.getElementById("magic-input").value = newmagicinput;
+  console.log(magicArray);
+  var newmagicinput = "";
+  magicArray.reverse();
+  for (let i = 0; i < magicArray.length; i++) {
+    if (i == magicArray.length - 1) {
+      newmagicinput += magicArray[i];
+    } else {
+      newmagicinput += magicArray[i] + "\n";
+    }
+  }
+  document.getElementById("magic-input").value = newmagicinput;
 };
 
 const stripBlank = () => {
@@ -50,7 +50,4 @@ const stripBlank = () => {
     newmagicinput += magicline.trim() + "\n";
   });
   document.getElementById("magic-input").value = newmagicinput;
-}
-
-
-
+};
